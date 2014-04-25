0eaf0e9dadd437923f56384a4f61e58579493a8f
Phil Wolpers <pwolpers@gmail.com>
2014-04-25 12:34:31 -0500

    Update new-window.js
    Description and how to use the function added as comments.

--- new-window.js
+++ new-window.js
@@ -1,3 +1,8 @@
+//This function allows you to automatically open links in a new window using <a href=""> in 
+//an XHTML/HTML5 compatible manner. 
+
+//use class="new-window" in your <a> tag.
+
 window.onload = function() {
 	var links = document.getElementsByTagName('a');
 	for (var i=0;i < links.length;i++) {
